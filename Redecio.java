5cf085c54aa84e0ac9fc62335a98e4c78dd3b0ae
Redecio <jmredecio@paterostechnologicalcollege.edu.ph>
2025-03-03 21:48:38 +0800

    Delete unwanted files

---
+++ Redecio.java
@@ -0,0 +1,8 @@
+public class Redecio {
+    public static void display() {
+        int age = 20; // Replace with user input if necessary
+        System.out.println("Hello! This is Redecio contributing to the project.");
+        System.out.println("My current age is: 19" + age);
+        System.out.println("Next year, I will be: 20" + (age + 1));
+    }
+}
