1db91f8807a4dd88a8eca1010c40d0a8ac1a3191
Redecio <jmredecio@paterostechnologicalcollege.edu.ph>
2025-04-07 18:19:16 +0800

    Redecio

--- AlimMaria.java
+++
@@ -1,8 +0,0 @@
-public class AlimMaria {
-    public static void display() {
-        int age = 21; // Replace with user input if necessary
-        System.out.println("Hello! This is Cruz contributing to the project.");
-        System.out.println("My current age is: " + 21);
-        System.out.println("Next year, I will be: " + (21 + 1));
-    }
-}
--- Esponga.java
+++
@@ -1,8 +0,0 @@
-public class Esponga {
-    public static void display() {
-        int age = 19;
-        System.out.println("Hello! My name Esponga, Stephen kyle contributing to the project.");
-        System.out.println("My current age is: " + age);
-        System.out.println("Next year, I will be: " + (age + 1));
-    }
-}
